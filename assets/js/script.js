7ad08718ef64b53d531fe822507dea030131c60e
AlejoCastroT <alejandro.castro@unac.edu.co>
2023-05-02 16:49:32 -0500

    nuevos

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,30 @@ window.onscroll = function(){
 
     header = document.getElementById("header");
 
-    if(scroll > 20 ){
+    if(scroll > 20){
         header.classList.add('menu_mod');
-    }else if (scroll < 20 ){
+    }else if(scroll < 20){
         header.classList.remove('menu_mod');
     }
-    
 }
 
+document.getElementById("btn_menu").addEventListener("click", mostrar_menu);
+
+menu = document.getElementById("header");
+body = document.getElementById("container_all");
+nav = document.getElementById("nav");
+
+function mostrar_menu(){
+    
+    menu.classList.toggle('move_content');
+    body.classList.toggle('move_content');
+    nav.classList.toggle('move_nav');
+}
+window.addEventListener("resize", function(){
+
+    if(window.innerWidth > 760){
+        menu.classList.remove('move_content');
+    body.classList.remove('move_content');
+    nav.classList.remove('move_nav');
+    }
+})    
